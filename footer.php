7d7197ff800cde234529062f34e61353d636a089
Emilio Dalla Torre <info@emiliodallatorre.it>
2022-04-11 23:33:48 +0200

    Footer sistemato.
    Took 1 hour 17 minutes

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="footer-columns">
+    <div class="footer-column">
+        <img src="https://mitionline.it/wp-content/uploads/2022/04/image.png" alt="">
+    </div>
+    <div class="footer-column">
+        <h4>La nostra sede</h4>
+        <p>Via Giuseppe Verdi, 50,<br>31010 Farra di Soligo TV</p>
+    </div>
+    <div class="footer-column">
+        <h4>Contatti</h4>
+        <p>T +39 0438 402887<br>F +39 0438 402568<br>E <a href="mailto:info@mitionline.it">info@mitionline.it</a></p>
+    </div>
+</div>
+<div class="footer-bar">
+    <p>© 2022 Miti Srl – Tutti i diritti riservati – P.IVA IT 012309430931</p>
+</div>
--- functions.php
+++ functions.php
@@ -6,3 +6,50 @@ function childtheme_parent_styles()
 }
 
 add_action('wp_enqueue_scripts', 'childtheme_parent_styles', 11);
+
+function register_widget_areas()
+{
+
+    register_sidebar(array(
+        'name' => 'Footer area one',
+        'id' => 'footer_area_one',
+        'description' => 'This widget area description',
+        'before_widget' => '<section class="footer-area footer-area-one">',
+        'after_widget' => '</section>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
+
+    register_sidebar(array(
+        'name' => 'Footer area two',
+        'id' => 'footer_area_two',
+        'description' => 'This widget area description',
+        'before_widget' => '<section class="footer-area footer-area-two">',
+        'after_widget' => '</section>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
+
+    register_sidebar(array(
+        'name' => 'Footer area three',
+        'id' => 'footer_area_three',
+        'description' => 'This widget area description',
+        'before_widget' => '<section class="footer-area footer-area-three">',
+        'after_widget' => '</section>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
+
+    register_sidebar(array(
+        'name' => 'Footer area four',
+        'id' => 'footer_area_four',
+        'description' => 'This widget area description',
+        'before_widget' => '<section class="footer-area footer-area-four">',
+        'after_widget' => '</section>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
+
+}
+
+add_action('widgets_init', 'register_widget_areas');
